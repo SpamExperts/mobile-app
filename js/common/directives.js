a66e5e1e27185990e64ca9dba26db60cdde8cf2b
nicolae <nicolae@spamexperts.com>
2016-04-19 10:53:46 +0300

    Added nicer checkboxes

--- js/common/directives.js
+++ js/common/directives.js
@@ -5,4 +5,11 @@ angular.module('SpamExpertsApp')
                 templateUrl: 'templates/common/messageQueue.html'
             };
         }
+    )
+    .directive('seCheckbox', function () {
+            return {
+                replace: true,
+                templateUrl: 'templates/common/checkbox.html'
+            };
+        }
     );
