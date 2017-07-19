db15833054f60517b339f89b2259830d15a7cdfe
Cristina Voineag <cristina@localhost.localdomain>
2017-07-19 15:00:04 +0300

    Add test to conf file

--- tests/mobileConf.js
+++ tests/mobileConf.js
@@ -1,6 +1,6 @@
 exports.config = {
       seleniumAddress: 'http://localhost:4444/wd/hub',
-      specs: ['elementCheck.js', 'datatypespec.js'],
+      specs: ['elementCheck.js', 'dataType.js', 'successfulLogin.js'],
       onPrepare: function () {	
          browser.driver.manage().window().setSize(1680, 1050);
       },
