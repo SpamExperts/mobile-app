dd05f11a6d8453a266675e0a6001d188302097a5
Cristina Voineag <cristina@localhost.localdomain>
2017-07-19 14:56:33 +0300

    Add new successfull login automated test and rename datatypespec.js

---
+++ tests/successfulLogin.js
@@ -0,0 +1,39 @@
+var PageData = function()   {
+
+    this.logo = browser.findElement(by.className('se-icon'));
+    this.hostname = browser.findElement(by.model('data.hostname'));
+    this.username = browser.findElement(by.model('data.username'));
+    this.password = browser.findElement(by.model('data.password'));
+    this.rememberButton = element.all(by.model('data.remember')).get(0);
+    this.logButton = browser.findElement(by.className('button button-block button-dark se-bold disable-user-behavior'));
+}
+
+var InputData = function(hostname, username, password)  {
+    this.hostname = hostname;
+    this.username = username;
+    this.password = password;
+}
+
+
+describe('Verify Data Type', function() {
+
+  it('Check:', function() {
+
+    // Open page
+    browser.get('http://localhost:8100/#/login'); 
+
+    var test = new PageData();
+    var data = new InputData("server1.test5.simplyspamfree.com", "mobile_app", "qwe123");
+
+    test.hostname.sendKeys(data.hostname);
+    test.username.sendKeys(data.username);
+    test.password.sendKeys(data.password);
+    test.rememberButton.click();
+    test.logButton.click();
+
+    browser.sleep(500);
+    var success = browser.findElement(by.className('dashboard'));
+    expect(success.getTagName()).toBe('div');
+  });
+
+});
