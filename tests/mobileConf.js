f6ace98c5bea7048cdc352a1d133bb769765384e
Cristina Voineag <cristina@localhost.localdomain>
2017-07-24 09:38:06 +0300

    New data case for test.

--- tests/mobileConf.js
+++ tests/mobileConf.js
@@ -1,11 +1,13 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['elementCheck.js',
+    specs: [
+        'elementCheck.js',
         'dataType.js',
         'keepLogin.js',
         'alertCheck.js',
         'inexistingUser.js',
-        'successfulLogin.js'
+        'successfulLogin.js',
+        'usersRestrictedLogin.js'
     ],
     onPrepare: function() {
         browser.driver.manage().window().setSize(1680, 1050);
