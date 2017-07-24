10ea38f5f64cc77c930e0ea7a9b02067cfe49557
George Sfrija <george@localhost.localdomain>
2017-07-24 16:01:00 +0300

    Update test.Implement only with xpath

--- tests/mobileConf.js
+++ tests/mobileConf.js
@@ -7,14 +7,15 @@ exports.config = {
         'alertCheck.js',
         'inexistingUser.js',
         'successfulLogin.js',
-        'usersRestrictedLogin.js'
+        'usersRestrictedLogin.js',
+        'dashPageButtons.js'
     ],
     onPrepare: function() {
         browser.driver.manage().window().setSize(1680, 1050);
     },
     capabilities: {
-        browserName: 'chrome',
-        name: 'Unnamed Job',
+        browserName: 'firefox',
+       /* name: 'Unnamed Job',
         count: 1,
         shardTestFiles: false,
         maxInstances: 10,
@@ -22,7 +23,7 @@ exports.config = {
             'mobileEmulation': {
                 'deviceName': 'Galaxy S5'
             }
-        }
+        }*/
     },
 
 
