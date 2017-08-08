7ae5cd3ca8730d7b1b53dc2151ae15eda48e14e9
Cristina Voineag <cristina@localhost.localdomain>
2017-08-08 16:55:32 +0300

    Add comments

--- tests/screen/authScreenKeepLogin.js
+++ tests/screen/authScreenKeepLogin.js
@@ -1,74 +1,93 @@
 var LoginPage = require('.././dependencies/LoginPageObject.js');
 var dashPage = require('.././dependencies/DashPageObject.js');
 
-function addCredentials(Obj, host, user, pwd) {
+function addCredentials(page, host, user, pwd) {
     //The three fields should be provided with valid data
-    Obj.hostname.sendKeys(host);
-    Obj.user.sendKeys(user);
-    Obj.password.sendKeys(pwd);
+    page.hostname.sendKeys(host);
+    page.user.sendKeys(user);
+    page.password.sendKeys(pwd);
 }
 
-function field_cleaner(Obj) {
-    Obj.hostname.clear();
-    Obj.password.clear();
-    Obj.user.clear();
+function field_cleaner(page) {
+    page.hostname.clear();
+    page.password.clear();
+    page.user.clear();
 }
+
+//  Load user data
 var data = require(".././dependencies/dataForUserRestrictedLogin");
 
-//The error message that is checked it's the one the application returns at the moment the test are written.
-//If there will be an update the error message could be changed depending on the
-//case it envolves.
-//Here should be a feature allowing the soft to detect an inexisting user
+/**
+ *  The error message that is checked it's the one the application returns at the moment the test are written.
+ *  If there will be an update the error message could be changed depending on the
+ *  case it envolves.
+ *  Here should be a feature allowing the soft to detect an inexisting user
+ */
+describe('Verify remember button', function() {
 
-describe('mobile app login page', function() {
-
-    var Obj = new LoginPage(); // initialize an object//
+    var page = new LoginPage();
     var alreadyLogged = new dashPage();
-    // var dashA = new dashAlert();
+    var EC = protractor.ExpectedConditions;
 
     it('should keep the user logged if the button is checked and not logged otherwise', function() {
 
+        //  Open app
         browser.get('http://localhost:8100/#/login');
-        field_cleaner(Obj);
-        //for being able to login, the .json file must have valid user, and password on the second element of the arrays.
-        addCredentials(Obj, data.superAdminH, data.superAdminU, data.superAdminP);
-        Obj.reminder.click();
-        Obj.logbutton.click();
-        var EC = protractor.ExpectedConditions;
 
-        browser.wait(EC.visibilityOf(alreadyLogged.bigLoginCheck), 20000)
-            .then(function() {
-                expect(alreadyLogged.bigLoginCheck.isPresent()).toBeTruthy();
+        //  Clear log in fields
+        field_cleaner(page);
 
-            });
+        //  For being able to login, the .json file must have valid user, and password on the second element of the arrays.
+        addCredentials(page, data.superAdminH, data.superAdminU, data.superAdminP);
+
+        //  Log in
+        page.reminder.click();
+        page.logbutton.click();
+
+        //  Check the login was successful and dashboard is visible
+        browser.wait(EC.visibilityOf(alreadyLogged.bigLoginCheck), 10000).then(function() {
+            expect(alreadyLogged.bigLoginCheck.isPresent()).toBeTruthy();
+        });
 
         browser.refresh();
-        browser.wait(EC.visibilityOf(alreadyLogged.bigLoginCheck), 20000)
-            .then(function() {
-                expect(alreadyLogged.bigLoginCheck.isPresent()).toBeTruthy();
-            });
-        alreadyLogged.leftButton.click();
 
+        //  Check the user is still logged 
+        browser.wait(EC.visibilityOf(alreadyLogged.bigLoginCheck), 10000).then(function() {
+            expect(alreadyLogged.bigLoginCheck.isPresent()).toBeTruthy();
+        });
+
+        //  Try log out
+        alreadyLogged.leftButton.click();
         alreadyLogged.logoutButton.click();
         alreadyLogged.cancelButton.click();
-
         alreadyLogged.logoutButton.click();
         alreadyLogged.okButton.click();
 
-        browser.wait(EC.visibilityOf(Obj.logbutton), 20000)
-            .then(function() {
-                field_cleaner(Obj);
-            });
-        Obj.reminder.click();
-        addCredentials(Obj, data.superAdminH, data.superAdminU, data.superAdminP);
+        //  Clear log in page
+        browser.wait(EC.visibilityOf(page.logbutton), 10000).then(function() {
+            field_cleaner(page);
+        });
 
-        Obj.logbutton.click();
-        browser.wait(EC.visibilityOf(alreadyLogged.bigLoginCheck), 20000)
-            .then(function() {
-                expect(alreadyLogged.bigLoginCheck.isPresent()).toBeTruthy();
-            });
+        //  Disable remember function
+        page.reminder.click();
+
+        //  Log in
+        addCredentials(page, data.superAdminH, data.superAdminU, data.superAdminP);
+        page.logbutton.click();
+
+        //  Check the login was successful and dashboard is visible
+        browser.wait(EC.visibilityOf(alreadyLogged.bigLoginCheck), 10000).then(function() {
+            expect(alreadyLogged.bigLoginCheck.isPresent()).toBeTruthy();
+        });
+
+        //  Check the user is not logged anymore
         browser.refresh();
-        field_cleaner(Obj);
+
+        //  Clear log in fields
+        field_cleaner(page);
+
+        //  Check remember button is unchecked
         expect(alreadyLogged.bigLoginCheck.isPresent()).toBeFalsy();
+
     });
 });
--- tests/screen/authScreenSuccessfulAtempt.js
+++ tests/screen/authScreenSuccessfulAtempt.js
@@ -13,7 +13,7 @@ describe('Verify Successful Login', function() {
 
         // Open page
         browser.get('http://localhost:8100/#/login');
-        browser.ignoreSynchronization = true;
+        browser.ignoreSynchronization = false;
 
         // Take elements
         test = new LoginPage();
@@ -21,29 +21,34 @@ describe('Verify Successful Login', function() {
 
         var EC = protractor.ExpectedConditions;
 
+        //  Clear log in page
         field_cleaner(test);
 
+        //  Load valid user data
         data = require('.././dependencies/dataForUserRestrictedLogin.json');
 
-        //  Login
-        field_cleaner(test);
+        //  Log in
         test.hostname.sendKeys(data.superAdminH);
         test.user.sendKeys(data.superAdminU);
         test.password.sendKeys(data.superAdminP);
         test.logbutton.click();
 
+        //  Check log in is successful
         browser.wait(EC.elementToBeClickable(dash.loginCheck), 5000).then(function() {
             expect(dash.loginCheck.isPresent()).toBe(true);
         });
 
+        //  Check left button
         browser.wait(EC.visibilityOf(dash.leftButton), 5000).then(function() {
             expect(dash.leftButton.isPresent()).toBe(true);
         });
 
+        //  Enter left menu
         browser.wait(EC.elementToBeClickable(dash.leftButton), 5000).then(function() {
             dash.leftButton.click();
         });
 
+        //  Log out
         browser.wait(EC.elementToBeClickable(dash.logoutButton), 5000).then(function() {
             dash.logoutButton.click();
         });
