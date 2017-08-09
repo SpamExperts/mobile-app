72561fa6939ad10bffa559ddfc77c06026ecabfa
George Sfrija <george@localhost.localdomain>
2017-08-09 09:09:22 +0300

    Updated dependecy

--- tests/dependencies/IncomingPageWithEmails.js
+++ tests/dependencies/IncomingPageWithEmails.js
@@ -1,3 +1,6 @@
+var msg1="Choosing 'Release and Train', for one or several messages, might adversely affect the ";
+var msg2="quality of filtering for all the existing users.Please avoid any mistakes in your selection!";
+var msg=msg1.concat(msg2);
 var imailButtons = function() {
     this.selectButton = element.all(by.model("message.isChecked")).get(0);
     this.releaseButton = element.all(by.css(".button.button-clear")).get(0);
@@ -16,7 +19,9 @@ var imailButtons = function() {
     this.mailDate = element.all(by.css(".col.col-80.message-title.ng-binding")).get(0);
     this.popup = element(by.cssContainingText(".ng-binding", "Please filter the search using a domain"));
     this.closePopup = element(by.xpath("//i[contains(@class,'icon ion-close-round icon-accessory disable-user-behavior')]"));
-
+    this.emailDeleteAction=element(by.cssContainingText('.ng-binding',"Deleting message(s). This might take a while depending on server load."));
+    this.emailReleaseAction=element(by.cssContainingText('.ng-binding','Message(s) released succesfully'));
+    this.emailRelAndTrain=element(by.cssContainingText('.ng-binding',msg));
 };
 
 module.exports = imailButtons;
